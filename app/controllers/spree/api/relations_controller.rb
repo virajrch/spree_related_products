40129c76ca656ff0575797595d4dcd119bfb7dfe
virajrch <virajz.c@gmail.com>
2023-03-06 13:32:55 +0530

    Update Spree API namespace to include V1

--- app/controllers/spree/api/relations_controller.rb
+++ app/controllers/spree/api/relations_controller.rb
@@ -1,6 +1,6 @@
 module Spree
   module Api
-    class RelationsController < Spree::Api::BaseController
+    class RelationsController < Spree::Api::v1::BaseController
       include ActionController::MimeResponds
 
       before_action :load_data, only: [:create, :destroy]
